90ad6a92cb6f35b7b4c2bbabe368a3321a37cc06
tusiim3 <tusiimejohnmark@gmail.com>
2026-05-05 15:36:42 +0300

    feat: install resend for emails

--- backend/apps/users/password_utils.py
+++ backend/apps/users/password_utils.py
@@ -1,27 +1,23 @@
-from django.core.mail import send_mail
+import resend
 from django.conf import settings
 from .models import PasswordResetToken
-import traceback
 
+resend.api_key = settings.RESEND_API_KEY
 
 def generate_and_send_token(user):
     PasswordResetToken.objects.filter(user=user, is_used=False).delete()
     reset_token = PasswordResetToken.objects.create(user=user)
-
     reset_url = f"{settings.FRONTEND_URL}/reset-password?token={reset_token.token}"
-    
-    try:
-        send_mail(
-            'ILES Password Reset Request',
+
+    resend.Emails.send({
+        "from": "ILES <onboarding@resend.dev>",
+        "to": [user.email],
+        "subject": "ILES Password Reset Request",
+        "text": (
             f'Hi {user.full_name},\n\n'
             f'You requested a password reset for your ILES account.\n\n'
             f'Use this link to reset your password:\n{reset_url}\n\n'
             f'This link expires in 30 minutes.\n\n'
-            f'If you did not request this, please ignore this email.',
-            settings.DEFAULT_FROM_EMAIL,
-            [user.email],
-            fail_silently=False,
+            f'If you did not request this, please ignore this email.'
         )
-    except Exception as e:
-        traceback.print_exc()
-        raise
+    })
