04e79cac8a18540f1703d24f9222ffe91edb0aa1
tusiim3 <tusiimejohnmark@gmail.com>
2026-05-06 06:20:26 +0300

    feat: add react tostify for pop up notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,28 @@ import { AuthProvider } from './context/AuthContext';
 import App from './App';
 import './styles/global.css';
 import './styles/list.css';                                               
+
+// Systemic Notification Imports
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
                                                                 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>                                                          
         <App />
+        <ToastContainer 
+          position="top-right" 
+          autoClose={3000} 
+          hideProgressBar={false} 
+          newestOnTop={false} 
+          closeOnClick 
+          rtl={false} 
+          pauseOnFocusLoss 
+          draggable 
+          pauseOnHover 
+          theme="colored" 
+        />
       </AuthProvider>                                                         
     </BrowserRouter>                                           
   </React.StrictMode>
